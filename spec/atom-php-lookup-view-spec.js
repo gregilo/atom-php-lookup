1f1f6a63ee02ae64570f926edf72fa95af2f7e87
Greg Aiello <gregoryaiello693@gmail.com>
2018-05-20 21:26:51 -0400

    Initial commit

---
+++ spec/atom-php-lookup-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPhpLookupView from '../lib/atom-php-lookup-view';
+
+describe('AtomPhpLookupView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
